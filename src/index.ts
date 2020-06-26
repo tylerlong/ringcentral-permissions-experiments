07c2576d6e015158530ce8cf49b1e7b9fad13696
Tyler Long <tyler4long@gmail.com>
2020-06-26 11:40:05 -0700

    Add pubnub sample code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import RingCentral from 'ringcentral-extensible';
+import PubNubExtension from 'ringcentral-extensible/build/extensions/pubNub';
+import waitFor from 'wait-for-async';
 
 const rc1 = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID!,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
   server: process.env.RINGCENTRAL_SERVER_URL!,
 });
+const pubNubExtension1 = new PubNubExtension();
+rc1.installExtension(pubNubExtension1);
+
 const rc2 = new RingCentral({
   clientId: process.env.RINGCENTRAL_CLIENT_ID!,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
   server: process.env.RINGCENTRAL_SERVER_URL!,
 });
+const pubNubExtension2 = new PubNubExtension();
+rc2.installExtension(pubNubExtension2);
 
 (async () => {
   await rc1.authorize({
@@ -23,13 +30,27 @@ const rc2 = new RingCentral({
     password: process.env.RINGCENTRAL_PASSWORD_2!,
   });
 
+  const ext1 = await rc1.restapi().account().extension().get();
+  const ext2 = await rc2.restapi().account().extension().get();
+
   const phoneNumber1 = (
     await rc1.restapi().account().extension().phoneNumber().get()
   ).records!.filter(pn => pn.features?.includes('SmsSender'))[0].phoneNumber!;
-
   const phoneNumber2 = (
     await rc2.restapi().account().extension().phoneNumber().get()
   ).records!.filter(pn => pn.features?.includes('SmsSender'))[1].phoneNumber!;
+
+  let count = 0;
+  pubNubExtension1.subscribe(
+    [
+      `/restapi/v1.0/account/~/extension/${ext1.id}/message-store/instant?type=SMS`,
+      `/restapi/v1.0/account/~/extension/${ext2.id}/message-store/instant?type=SMS`,
+    ],
+    event => {
+      console.log(JSON.stringify(event, null, 2));
+      count += 1;
+    }
+  );
 
   await rc1
     .restapi()
@@ -52,6 +73,10 @@ const rc2 = new RingCentral({
       to: [{phoneNumber: phoneNumber1}],
       text: 'hello world',
     });
+
+  await waitFor({condition: () => count > 1, interval: 1000, times: 30});
+
+  console.log(count);
 
   await rc1.revoke();
   await rc2.revoke();
